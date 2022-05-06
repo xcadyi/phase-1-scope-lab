da6dc107e78466c087d2b7c1c9527a9a4e5335f0
Selin Ayyildiz <xcadyix@gmail.com>
2022-05-06 17:32:22 -0400

    commit tree 1dd6cf986a459949acc11c22c4a2b40446c4dfe4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob'
+  return bestCustomer
+}
